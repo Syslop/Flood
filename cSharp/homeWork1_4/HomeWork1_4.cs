23b78ecc307d5cf5ec3ebdb9b41ad0f3e8b730ec
Syslop <petrovkonstantined@gmail.com>
2022-09-05 02:57:39 +0300

    Create file homework 1 task 8. Added run programm with user.

---
+++ cSharp/homeWork1_4/HomeWork1_4.cs
@@ -0,0 +1,18 @@
+﻿//for hardcode
+/*
+int firstNumber = 57;
+*/
+
+//for code with user
+Console.WriteLine("Please enter number:");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("All even numbers up to the entered number:");
+
+int startNumber = 2;
+
+while (startNumber <= firstNumber)
+{
+    Console.Write(startNumber);
+    Console.Write("  ");
+    startNumber += 2;
+}
