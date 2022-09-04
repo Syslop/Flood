9521d1c090a8f4644ccae33fd622c25485f27dd6
Syslop <petrovkonstantined@gmail.com>
2022-09-05 02:18:02 +0300

    Create file for homework 1.

---
+++ cSharp/homeWork1_1/HomeWork1_1.cs
@@ -0,0 +1,19 @@
+﻿int firstNumber = 8;
+int secondNumber = 9;
+
+if (firstNumber > secondNumber)
+{
+    Console.Write("max = firstNumber = ");
+    Console.Write(firstNumber);
+    Console.Write("; min = secondNumber = ");
+    Console.Write(secondNumber);
+    Console.Write(".");
+} 
+else
+{
+    Console.Write("max = secondNumber = ");
+    Console.Write(secondNumber);
+    Console.Write("; min = firstNumber = ");
+    Console.Write(firstNumber);
+    Console.Write(".");
+}
