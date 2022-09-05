05905b77852f09c832a6d38e292f7b10b08be6a3
Syslop <petrovkonstantined@gmail.com>
2022-09-05 17:51:41 +0300

    Create file homework 2 task 10. Added run programm with random number.

---
+++ cSharp/homeWork2_1/HomeWork2_1.cs
@@ -0,0 +1,20 @@
+﻿/* task 10
+
+Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1
+
+*/
+
+int randomNumber = new Random().Next(100, 1000);
+
+Console.Write("Random number = ");
+Console.Write(randomNumber);
+Console.WriteLine();
+
+int secondNum = (randomNumber / 10) % 10;
+
+Console.Write("A desired (second) number = ");
+Console.Write(secondNum);
