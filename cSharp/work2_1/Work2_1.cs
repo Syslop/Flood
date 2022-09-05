305d83ec20f4ef2485469def78c1d2e3557385b8
Syslop <petrovkonstantined@gmail.com>
2022-09-05 15:01:13 +0300

    Lesson 2. Create file task 9.

---
+++ cSharp/work2_1/Work2_1.cs
@@ -0,0 +1,20 @@
+﻿int randomNumber = new Random().Next(10, 100);
+
+Console.Write("Random number = ");
+Console.Write(randomNumber);
+Console.WriteLine();
+
+int firstNum = randomNumber / 10;
+int secondNum = randomNumber % 10;
+
+
+if (firstNum > secondNum) 
+{
+    Console.Write("Max number = first number = ");
+    Console.Write(firstNum);
+}
+else 
+{
+    Console.Write("Max number = second number = ");
+    Console.Write(secondNum);
+}
