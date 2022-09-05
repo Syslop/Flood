7ed2b268bcf9d63d0aa2a930a07b2b38ca42e5ad
Syslop <petrovkonstantined@gmail.com>
2022-09-05 23:00:39 +0300

    Update file homework 2 task 15. Added method FindWeekend.

--- cSharp/homeWork2_3/HomeWork2_3.cs
+++ cSharp/homeWork2_3/HomeWork2_3.cs
@@ -7,25 +7,30 @@
 1 -> нет
 
 */
+
 Console.WriteLine("Please enter number day of week: ");
+FindWeekend();
 
-bool correctnessFlag = true;
-while (correctnessFlag)
+void FindWeekend()
     {
-    int dayNumber = Convert.ToInt32(Console.ReadLine());
+    bool correctnessFlag = true;
+    while (correctnessFlag)
+        {
+        int dayNumber = Convert.ToInt32(Console.ReadLine());
 
-    if (dayNumber < 1 || dayNumber > 7) 
-    {
-        Console.WriteLine("Please enter correct number day of week: ");
-    } 
-    else if (dayNumber == 6 || dayNumber == 7) 
-    {
-        Console.WriteLine("Day is weekend.");
-        correctnessFlag = false;
-    } 
-    else
-    {
-        Console.WriteLine("Day is not weekend.");
-        correctnessFlag = false;
+        if (dayNumber < 1 || dayNumber > 7) 
+        {
+            Console.WriteLine("Please enter correct number day of week: ");
+        } 
+        else if (dayNumber == 6 || dayNumber == 7) 
+        {
+            Console.WriteLine("Day is weekend.");
+            correctnessFlag = false;
+        } 
+        else
+        {
+            Console.WriteLine("Day is not weekend.");
+            correctnessFlag = false;
+        }
     }
 }
