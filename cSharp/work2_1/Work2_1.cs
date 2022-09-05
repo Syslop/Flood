ceb4b4a17b4d4cd632d8b56443dcfa0d58b820a8
Syslop <petrovkonstantined@gmail.com>
2022-09-05 15:08:59 +0300

    Lesson 2. Added method MinMax.

--- cSharp/work2_1/Work2_1.cs
+++ cSharp/work2_1/Work2_1.cs
@@ -1,4 +1,18 @@
-﻿int randomNumber = new Random().Next(10, 100);
+﻿int MinMax(int num1, int num2) 
+{
+    int max = 0;
+    if (num1 > num2) 
+    {
+        max = num1;
+    }
+    else 
+    {
+        max = num2;
+    }
+    return max;
+}
+
+int randomNumber = new Random().Next(10, 100);
 
 Console.Write("Random number = ");
 Console.Write(randomNumber);
@@ -7,14 +21,5 @@ Console.WriteLine();
 int firstNum = randomNumber / 10;
 int secondNum = randomNumber % 10;
 
-
-if (firstNum > secondNum) 
-{
-    Console.Write("Max number = first number = ");
-    Console.Write(firstNum);
-}
-else 
-{
-    Console.Write("Max number = second number = ");
-    Console.Write(secondNum);
-}
+Console.Write("Max number = ");
+Console.Write(MinMax(firstNum, secondNum));
