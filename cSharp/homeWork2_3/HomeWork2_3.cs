a7b93f34e10966f3454eaa21eb2d2460ef3f522d
Syslop <petrovkonstantined@gmail.com>
2022-09-05 18:04:16 +0300

    Create file homework 2 task 15. Added run programm with user.

---
+++ cSharp/homeWork2_3/HomeWork2_3.cs
@@ -0,0 +1,31 @@
+﻿/* task 15
+
+Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+6 -> да
+7 -> да
+1 -> нет
+
+*/
+Console.WriteLine("Please enter number day of week: ");
+
+bool correctnessFlag = true;
+while (correctnessFlag)
+    {
+    int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+    if (dayNumber < 1 || dayNumber > 7) 
+    {
+        Console.WriteLine("Please enter correct number day of week: ");
+    } 
+    else if (dayNumber == 6 || dayNumber == 7) 
+    {
+        Console.WriteLine("Day is weekend.");
+        correctnessFlag = false;
+    } 
+    else
+    {
+        Console.WriteLine("Day is not weekend.");
+        correctnessFlag = false;
+    }
+}
