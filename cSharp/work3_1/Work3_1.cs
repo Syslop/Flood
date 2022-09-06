3b3b4636517a7a03477248063ef6a9d5bb0e0496
Syslop <petrovkonstantined@gmail.com>
2022-09-06 20:46:36 +0300

    Create file work 3 task 18. Added code.

--- cSharp/work3_1/Work3_1.cs
+++ cSharp/work3_1/Work3_1.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* task 18
+
+Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+
+*/
+
+Console.WriteLine("Please enter quarter number: ");
+int quarterNumber = Convert.ToInt32(Console.ReadLine());
+
+getCoordinate(quarterNumber);
+
+void getCoordinate(int quarterNumber)
+{
+    if (quarterNumber == 1) 
+    {
+        Console.WriteLine("x > 0, y > 0"); 
+    } 
+    else if (quarterNumber == 2) 
+    {
+        Console.WriteLine("x < 0, y > 0"); 
+    } 
+    else if (quarterNumber == 3) 
+    {
+        Console.WriteLine("x < 0, y < 0"); 
+    } 
+    else if (quarterNumber == 4) 
+    {
+        Console.WriteLine("x > 0, y < 0"); 
+    }
+    else
+    {
+        Console.WriteLine("Please enter the correct quarter!"); 
+    } 
+}
