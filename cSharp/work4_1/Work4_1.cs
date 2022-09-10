d4dea11c4387d553d0cdb1ce32dd4d9ca495aa4e
Syslop <petrovkonstantined@gmail.com>
2022-09-10 11:25:05 +0300

    Update file work 4 task 1. Added run code with user.

--- cSharp/work4_1/Work4_1.cs
+++ cSharp/work4_1/Work4_1.cs
@@ -7,3 +7,21 @@
 
 
 */
+
+Console.WriteLine("Please enter number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Count number = {GetCountNumber()}");
+
+int GetCountNumber()
+{
+    int countNumber = 1;
+    int wholePart = number / 10;
+
+    while (wholePart != 0)
+    {
+        wholePart = wholePart / 10;
+        countNumber++;
+    }
+    return countNumber;
+}
