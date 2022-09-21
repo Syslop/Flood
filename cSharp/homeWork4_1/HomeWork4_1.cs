056f94891db371119c6da42391eb5c3b7e63961a
Syslop <petrovkonstantined@gmail.com>
2022-09-22 00:50:44 +0300

    Update file homework 4 task 25. Added a solution to the problem with user

--- cSharp/homeWork4_1/HomeWork4_1.cs
+++ cSharp/homeWork4_1/HomeWork4_1.cs
@@ -6,3 +6,16 @@
 2, 4 -> 16
 
 */
+
+Console.WriteLine("Please enter number A:");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Please enter number B:");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+RaiseNumberToPower(numberA, numberB);
+
+void RaiseNumberToPower(int number, int power)
+{
+    Console.WriteLine($"The number {number} in the power {power} is equal to {Math.Pow(number, power)}");
+}
