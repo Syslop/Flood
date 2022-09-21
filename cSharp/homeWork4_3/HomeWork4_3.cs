3d0317f8d521155877863a3ead0dbaf64b11db36
Syslop <petrovkonstantined@gmail.com>
2022-09-22 01:27:53 +0300

    Update file homework 4 task 29. Added a solution to the problem with user

--- cSharp/homeWork4_3/HomeWork4_3.cs
+++ cSharp/homeWork4_3/HomeWork4_3.cs
@@ -6,3 +6,37 @@
 6, 1, 33 -> [6, 1, 33]
 
 */
+
+int[] arrayNumber = new int[8];
+
+ArrayPrint(arrayNumber);
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine("Array after fill:");
+ArrayFill(arrayNumber);
+Console.WriteLine();
+ArrayPrint(arrayNumber);
+
+void ArrayFill(int[] arrayNumber)
+{
+    for (int i = 0; i < arrayNumber.Length; i++)
+    {
+        arrayNumber[i] = new Random().Next(1, 100);
+        Console.WriteLine($"Element №{i} equal {arrayNumber[i]}");
+    }
+}
+
+void ArrayPrint(int[] arrayNumber)
+{
+    for (int i = 0; i < arrayNumber.Length; i++)
+    {
+        Console.Write(arrayNumber[i]);
+
+        if (i == arrayNumber.Length - 1) 
+        {
+            break;
+        }
+
+        Console.Write(", ");
+    }
+}
