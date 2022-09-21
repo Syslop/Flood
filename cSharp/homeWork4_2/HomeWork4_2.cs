34231e1019aebc0c542d92b8749c7724c5c54555
Syslop <petrovkonstantined@gmail.com>
2022-09-22 01:02:28 +0300

    Update file homework 4 task 27. Added a solution to the problem with user

--- cSharp/homeWork4_2/HomeWork4_2.cs
+++ cSharp/homeWork4_2/HomeWork4_2.cs
@@ -7,3 +7,22 @@
 9012 -> 12
 
 */
+
+Console.WriteLine("Please enter number:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"The sum of all digits in the number {number} is equal to {GetSumNum(number)}");
+
+int GetSumNum(int number)
+{
+    int sum = number % 10;
+    int wholePart = number / 10;
+
+    while (wholePart != 0)
+    {
+        sum = sum + wholePart % 10;
+        wholePart = wholePart / 10;
+    }
+
+    return sum;
+}
