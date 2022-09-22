b941a941303f37106e7c17e7d2f6a02dd8a1ab12
Syslop <petrovkonstantined@gmail.com>
2022-09-22 23:48:09 +0300

    Update file homework 5 task 38. Added method ArrayFill.

--- cSharp/homeWork5_3/HomeWork5_3.cs
+++ cSharp/homeWork5_3/HomeWork5_3.cs
@@ -6,3 +6,21 @@
 
 */
 
+Console.WriteLine("Please enter array length: ");
+int arrayLength = Convert.ToInt32(Console.ReadLine());
+
+double[] arrayNumber = new double[arrayLength];
+
+Console.WriteLine("Array after fill:");
+ArrayFill(arrayNumber);
+Console.WriteLine();
+
+void ArrayFill(double[] arrayNumber)
+{
+    for (int i = 0; i < arrayNumber.Length; i++)
+    {
+        double number = new Random().NextDouble();
+        arrayNumber[i] = Math.Round(number * 10000) / 100.0;
+        Console.Write($"{arrayNumber[i]} ");
+    }
+}
