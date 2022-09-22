e1f06e49984f37d59db63a64bcb4c032c2e37393
Syslop <petrovkonstantined@gmail.com>
2022-09-23 00:13:47 +0300

    Update file homework 6 task 41. Added method GetQuantityGreaterZero.

--- cSharp/homeWork6_1/HomeWork6_1.cs
+++ cSharp/homeWork6_1/HomeWork6_1.cs
@@ -6,3 +6,25 @@
 1, -7, 567, 89, 223-> 3
 
 */
+
+Console.WriteLine("Please enter count number: ");
+int countNumber = Convert.ToInt32(Console.ReadLine());
+
+GetQuantityGreaterZero(countNumber);
+
+void GetQuantityGreaterZero(int countNumber)
+{
+    int quantityGreaterZero = 0;
+    int sequenceNumber = 1;
+    while (sequenceNumber <= countNumber)
+    {
+        Console.WriteLine($"Please enter your {sequenceNumber} number:");
+        int number = Convert.ToInt32(Console.ReadLine());
+        if (number > 0)
+        {
+            quantityGreaterZero++;
+        }
+        sequenceNumber++;
+    }
+    Console.WriteLine($"The user entered {quantityGreaterZero} numbers greater than zero");
+}
