8cd7fdbce9d24be685b0c99a0b7d472bef0c6b51
Syslop <petrovkonstantined@gmail.com>
2022-09-22 23:17:10 +0300

    pdate file homework 5 task 34. Added method ArrayFill.

--- cSharp/homeWork5_1/HomeWork5_1.cs
+++ cSharp/homeWork5_1/HomeWork5_1.cs
@@ -1,7 +1,28 @@
 ﻿/* Task 34
 
-Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, 
+которая покажет количество чётных чисел в массиве.
 
 [345, 897, 568, 234] -> 2
 
 */
+Console.WriteLine("Please enter array length: ");
+int arrayLength = Convert.ToInt32(Console.ReadLine());
+
+int[] arrayNumber = new int[arrayLength];
+
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine("Array after fill:");
+ArrayFill(arrayNumber);
+Console.WriteLine();
+
+void ArrayFill(int[] arrayNumber)
+{
+    for (int i = 0; i < arrayNumber.Length; i++)
+    {
+        arrayNumber[i] = new Random().Next(100, 1000);
+        Console.Write($"{arrayNumber[i]} ");
+    }
+}
+
