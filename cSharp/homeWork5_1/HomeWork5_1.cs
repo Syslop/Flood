3056199120e5bb7506c7c9ab7efbaeb20edfc3a3
Syslop <petrovkonstantined@gmail.com>
2022-09-22 23:24:43 +0300

    pdate file homework 5 task 34. Added method GetCountEvenNumbers.

--- cSharp/homeWork5_1/HomeWork5_1.cs
+++ cSharp/homeWork5_1/HomeWork5_1.cs
@@ -11,11 +11,10 @@ int arrayLength = Convert.ToInt32(Console.ReadLine());
 
 int[] arrayNumber = new int[arrayLength];
 
-Console.WriteLine();
-Console.WriteLine();
 Console.WriteLine("Array after fill:");
 ArrayFill(arrayNumber);
 Console.WriteLine();
+GetCountEvenNumbers(arrayNumber);
 
 void ArrayFill(int[] arrayNumber)
 {
@@ -24,5 +23,18 @@ void ArrayFill(int[] arrayNumber)
         arrayNumber[i] = new Random().Next(100, 1000);
         Console.Write($"{arrayNumber[i]} ");
     }
+}
+
+void GetCountEvenNumbers(int[] arrayNumber)
+{
+    int count = 0;
+    for (int i = 0; i < arrayNumber.Length; i++)
+    {
+        if ((arrayNumber[i] % 2) == 0) 
+        {
+            count++;
+        }
+    }
+    Console.WriteLine($"Count of even number = {count}");
 }
 
