2f57fef25d05eb12a527dcd44afba0188e778676
Syslop <petrovkonstantined@gmail.com>
2022-09-28 22:36:36 +0300

    Update file homework 7 task 47. Added methods FillArrayRandomNumbers and PrintArray.

--- cSharp/homeWork7_1/HomeWork7_1.cs
+++ cSharp/homeWork7_1/HomeWork7_1.cs
@@ -8,3 +8,36 @@ m = 3, n = 4.
 8 7,8 -7,1 9
 
 */
+
+Console.WriteLine("Please enter number of rows: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Please enter number of columns: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+double[,] array = new double[rows, columns];
+
+FillArrayRandomNumbers(array);
+PrintArray(array);
+
+void FillArrayRandomNumbers(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Math.Round(new Random().NextDouble() * 100, 2);
+        }
+    }
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
