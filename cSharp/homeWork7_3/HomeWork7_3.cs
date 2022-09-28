f85edc3ee07bc451ad4d0776a89eb9987770fc46
Syslop <petrovkonstantined@gmail.com>
2022-09-29 00:11:50 +0300

    Update file homework 7 task 52. Added method GetArithmeticMeanOfColumn.

--- cSharp/homeWork7_3/HomeWork7_3.cs
+++ cSharp/homeWork7_3/HomeWork7_3.cs
@@ -9,3 +9,63 @@
 Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
 
 */
+
+Console.WriteLine("Please enter number of rows: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Please enter number of columns: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = new int[rows, columns];
+
+int[] arrayArithmeticMean = new int[array.GetLength(1)];
+
+
+FillArrayRandomNumbers(array);
+PrintArray(array);
+GetArithmeticMeanOfColumn(array);
+Console.WriteLine();
+
+
+for (int i = 0; i < arrayArithmeticMean.Length; i++)
+{
+    Console.WriteLine(arrayArithmeticMean[i]);
+}
+
+
+void FillArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(10, 100);
+        }
+    }
+} 
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void GetArithmeticMeanOfColumn(int[,] array)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            arrayArithmeticMean[j] += array[i, j];
+        }
+    }
+    for (int i = 0; i < arrayArithmeticMean.Length; i++)
+    {
+        arrayArithmeticMean[i] = arrayArithmeticMean[i] / array.GetLength(0);
+    }
+}
