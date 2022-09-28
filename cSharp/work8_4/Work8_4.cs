5221d43d175c3b2753efbdc25b180c192d225224
Syslop <petrovkonstantined@gmail.com>
2022-09-28 22:24:40 +0300

    Update lesson 8 task 59. Comment 2 var.

---
+++ cSharp/work8_4/Work8_4.cs
@@ -0,0 +1,71 @@
+﻿/*Task 59
+
+Задайте двумерный массив из целых чисел. Напишите программу, которая удалит строку и столбец, 
+на пересечении которых расположен наименьший элемент массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+5 2 6 7
+Наименьший элемент - 1, на выходе получим
+следующий массив:
+9 4 2
+2 2 6
+3 4 7
+
+*/
+
+Console.WriteLine("Please enter count rows: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Please enter count columns: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = new int[rows, columns];
+
+FillArrayRandomNumbers(array);
+PrintArray(array);
+
+Console.WriteLine();
+
+int rowMinElement = 0;
+int columnMinElement = 0;
+int min = array[0, 0];
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        if (array[i, j] < min)
+        {
+            min = array[i, j];
+            rowMinElement = i;
+            columnMinElement = j;
+        }
+        array[i, j] = new Random().Next(0, 10);
+    }
+}
+
+
+void FillArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
