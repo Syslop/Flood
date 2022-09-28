83a9473f46a4a5c0a733ec0cfa919bea088fa50a
Syslop <petrovkonstantined@gmail.com>
2022-09-28 22:20:33 +0300

    Update lesson 8 task 55. Del rubbish.

---
+++ cSharp/work8_2/Work8_2.cs
@@ -0,0 +1,64 @@
+﻿/*Task 55
+
+Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы. 
+В случае, если это невозможно, программа должна вывести сообщение для пользователя.
+
+*/
+
+Console.WriteLine("Please enter count rows: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Please enter count columns: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = new int[rows, columns];
+
+FillArrayRandomNumbers(array);
+PrintArray(array);
+
+Console.WriteLine();
+PrintReveseArray(array);
+
+if (rows != columns)
+{
+    Console.WriteLine("I do not revers array.");
+} 
+else 
+{
+    PrintReveseArray(array);
+}
+
+void PrintReveseArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[j,i] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(-100, 100) / 10;
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
