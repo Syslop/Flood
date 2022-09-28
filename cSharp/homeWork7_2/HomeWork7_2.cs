acd7c0c814ef54d92fc1ae63f29d69df95a002af
Syslop <petrovkonstantined@gmail.com>
2022-09-28 22:57:24 +0300

    Update file homework 7 task 47. Added methods FillArrayRandomNumbers and PrintArray.

--- cSharp/homeWork7_2/HomeWork7_2.cs
+++ cSharp/homeWork7_2/HomeWork7_2.cs
@@ -9,3 +9,59 @@
 17 -> такого числа в массиве нет
 
 */
+
+Console.WriteLine("Please enter number of rows: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Please enter number of columns: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Please enter number row element: ");
+int elementRow = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Please enter number column element: ");
+int elementColumn = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = new int[rows, columns];
+
+int maxRow = array.GetLength(0);
+int maxColumn = array.GetLength(1);
+
+if (elementRow <= maxRow && elementColumn <= maxColumn)
+{
+    FillArrayRandomNumbers(array);
+    PrintArray(array);
+    Console.WriteLine(GetElement(array, elementRow - 1, elementColumn - 1));
+} 
+else
+{
+    Console.WriteLine("This element not found.");
+}
+
+
+void FillArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(10, 100);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int GetElement(int[,] array, int row, int column)
+{
+    int element = array[row, column];
+    return element;
+}
