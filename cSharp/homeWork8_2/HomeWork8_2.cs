3c3639d03793b3813dfff445b2c01e6ff3b1e1a7
Syslop <petrovkonstantined@gmail.com>
2022-09-29 22:48:50 +0300

    Create file homework 8 task 56. Added methods FillArrayRandomNumbers, PrintArray and GetMinSumArrayRowElement.

--- cSharp/homeWork8_2/HomeWork8_2.cs
+++ cSharp/homeWork8_2/HomeWork8_2.cs
@@ -12,3 +12,64 @@
 
 */
 
+Console.WriteLine("Please enter number of rows: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Please enter number of columns: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+while (rows == columns)
+{
+    Console.WriteLine("Please change number of columns to enter rectangular matrix: ");
+    columns = Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] array = new int[rows, columns];
+
+
+FillArrayRandomNumbers(array);
+PrintArray(array);
+Console.WriteLine();
+GetMinSumArrayRowElement(array);
+
+void FillArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void GetMinSumArrayRowElement(int[,] array)
+{
+    int minSumArrayElementInRow = 0;
+    int numberRowWithMinSumArrayElement = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        int sumArrayElementInRow = 0;
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            sumArrayElementInRow += array[i, j];
+        }
+        if (i == 0 || (sumArrayElementInRow < minSumArrayElementInRow))
+        {
+            minSumArrayElementInRow = sumArrayElementInRow;
+            numberRowWithMinSumArrayElement = i;
+        }
+    }
+    Console.WriteLine($"Row with minimal sum element = {numberRowWithMinSumArrayElement}. Sum = {minSumArrayElementInRow}");
+}
