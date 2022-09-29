9a2266a8b3165ce680f5a29b0b28635358207023
Syslop <petrovkonstantined@gmail.com>
2022-09-30 01:10:51 +0300

    Update file work 9 task 63 and 65. Del rubbish.

---
+++ cSharp/work9_1/Work9_1.cs
@@ -0,0 +1,28 @@
+﻿/*Task 63
+
+Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+N = 5 -> "1, 2, 3, 4, 5"
+N = 6 -> "1, 2, 3, 4, 5, 6"
+
+Task 65
+Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+M = 1; N = 5 -> "1, 2, 3, 4, 5"
+M = 4; N = 8 -> "4, 6, 7, 8"
+
+*/
+
+Console.WriteLine("Please enter number start: ");
+int start = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Please enter number end: ");
+int end = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(PrintNumber(start, end));
+
+string PrintNumber(long start, long end)
+{
+    if (start == end)
+    {
+        return start.ToString();
+    }
+    return(start + "," + PrintNumber(start + 1, end));
+}
