f7311230d6b961fe6e50816a42903d9b8bf49c09
Syslop <petrovkonstantined@gmail.com>
2022-09-30 02:11:32 +0300

    Update file homework 9 task 68. Added method GetAkkermanValue.

--- cSharp/homeWork9_3/HomeWork9_3.cs
+++ cSharp/homeWork9_3/HomeWork9_3.cs
@@ -5,3 +5,27 @@ m = 2, n = 3 -> A(m,n) = 9
 m = 3, n = 2 -> A(m,n) = 29
 
 */
+
+Console.WriteLine("Please enter M: ");
+int numM = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Please enter N: ");
+int numN = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Value of Akkerman function equals = {GetAkkermanValue(numM, numN)}");
+
+int GetAkkermanValue(int numM, int numN)
+{
+    if (numM == 0)
+    {
+        return numN + 1;
+    }
+    else if (numM > 0 && numN == 0)
+    {
+        return GetAkkermanValue((numM - 1), 1);
+    }
+    else if (numM > 0 && numN > 0)
+    {
+        return GetAkkermanValue((numM - 1), GetAkkermanValue(numM, (numN - 1)));
+    }
+    return -1;
+}
