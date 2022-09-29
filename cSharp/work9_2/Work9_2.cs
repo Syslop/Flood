5ea9bd5e149e7ea2a483430adf4b74bba96abce8
Syslop <petrovkonstantined@gmail.com>
2022-09-30 01:13:52 +0300

    Update file work 9 task 67. Added method PrintNumber.

---
+++ cSharp/work9_2/Work9_2.cs
@@ -0,0 +1,20 @@
+﻿/*Task 67
+
+Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+453 -> 12
+45 -> 9
+
+*/
+
+Console.WriteLine("Enter number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine(PrintNumber(num));
+int PrintNumber(int num)
+{
+    if (num == 0)
+    {
+        return 0;
+    }
+    return(num % 10 + PrintNumber(num / 10));
+}
