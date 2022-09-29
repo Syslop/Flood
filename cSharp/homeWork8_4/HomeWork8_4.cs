cdc1fd1586fd0e11f293fb468c7349d327cd17f7
Syslop <petrovkonstantined@gmail.com>
2022-09-29 23:14:52 +0300

    Create file homework 8 task 60. Added methods FillMatrix, PrintArray and FindElement.

--- cSharp/homeWork8_4/HomeWork8_4.cs
+++ cSharp/homeWork8_4/HomeWork8_4.cs
@@ -9,3 +9,71 @@
 26(1,0,1) 55(1,1,1)
 
 */
+
+Console.WriteLine("Enter number of rowsX for first matrix: ");
+int rowsX = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter number of rowsY for first matrix: ");
+int rowsY = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter number of rowsZ for first matrix: ");
+int rowsZ = Convert.ToInt32(Console.ReadLine());
+
+int[,,] matrix1 = new int[rowsX, rowsY, rowsZ];
+
+PrintArray(FillMatrix(matrix1));
+
+int[,,] FillMatrix(int[,,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            int k = 0;
+            while (k < arr.GetLength(2))
+            {
+                int number = new Random().Next(10, 99);
+                if (FindElement(arr, number))
+                {
+                    continue;
+                }
+                arr[i, j, k] = number;
+                k++;
+            }
+        }
+    }
+    return arr;
+}
+
+
+bool FindElement(int[,,] arr, int num)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            for (int k = 0; k < arr.GetLength(2); k++)
+            {
+                if (arr[i, j, k] == num)
+                {
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
+}
+
+void PrintArray(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                Console.Write($"{array[i, j, k]} ({i},{j},{k})   ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+}
