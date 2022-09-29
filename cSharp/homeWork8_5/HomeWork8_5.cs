9e651d049f17170b7d9843b2f3e267543e786cfe
Syslop <petrovkonstantined@gmail.com>
2022-09-30 00:51:45 +0300

    Update file homework 8 task 62. Added methods PrintArray and GetSpireArray.

--- cSharp/homeWork8_5/HomeWork8_5.cs
+++ cSharp/homeWork8_5/HomeWork8_5.cs
@@ -9,3 +9,58 @@
 10 09 08 07
 
 */
+
+Console.WriteLine("Please enter size of array (if 4x4 then size =4): ");
+int sizeArray = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = new int[sizeArray, sizeArray];
+
+PrintArray(GetSpireArray(sizeArray));
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] GetSpireArray(int sizeArray)
+{
+    var spireArray = new int[sizeArray, sizeArray];
+    for (int currentChar = 1, padding = 0; padding < sizeArray / 2; padding++)
+    {
+        for (int j = padding; j < sizeArray - padding; j++)
+        {
+            spireArray[padding, j] = currentChar;
+            currentChar++;
+        }
+
+        for (int i = padding + 1; i < sizeArray - padding - 1; i++)
+        {
+            spireArray[i, sizeArray - padding - 1] = currentChar;
+            currentChar++;
+        }
+
+        for (int j = padding; j < sizeArray - padding; j++)
+        {
+            spireArray[sizeArray - padding - 1, sizeArray - j - 1] = currentChar;
+            currentChar++;
+        }
+
+        for (int i = padding + 1; i < sizeArray - padding - 1; i++)
+        {
+            spireArray[sizeArray - i - 1, padding] = currentChar;
+            currentChar++;
+        }
+    }
+    if (sizeArray % 2 != 0) 
+    {
+        spireArray[(sizeArray)/2, (sizeArray)/2] = spireArray[(sizeArray)/2, (sizeArray - 2)/2] + 1;
+    }
+    return spireArray;
+}
